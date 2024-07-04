337bfa9f12583590af2842f2bbfde2e5089434e3
Mark A. Jensen <mark.jensen@nih.gov>
2024-07-04 17:10:04 -0400

    fix for null matchedResult

--- src/components/ModelNavigator/DataDictionary/Table/DataDictionaryNode/components/NodeViewComponent.jsx
+++ src/components/ModelNavigator/DataDictionary/Table/DataDictionaryNode/components/NodeViewComponent.jsx
@@ -40,7 +40,7 @@ const NodeViewComponent = ({
   );
 
   const getTitle = () => {
-    if (isSearchMode) {
+    if (isSearchMode && matchedResult) {
       const nodeTitleFragment = getNodeTitleFragment(
         matchedResult.matches,
         _.capitalize(node.handle),
@@ -52,7 +52,7 @@ const NodeViewComponent = ({
   };
 
   const getDescription = (description) => {
-    if (isSearchMode) {
+    if (isSearchMode && matchedResult) {
       const nodeDescriptionFragment = getNodeDescriptionFragment(
         matchedResult.matches,
         description,
